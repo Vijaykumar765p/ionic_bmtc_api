edd43fd8ac2ab07ea408a1e192a6a323c248f8c1
Vijaykumar765p <vpattar6@gmail.com>
2018-07-05 16:33:56 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,120 @@
+var express = require('express');
+var app = express();
+var http = require('http');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var configdata = require('./config/config');
+const cors = require('cors');
+//var common_function = require('./lib/common_function');
+var dbmodel = require('./lib/dbquery');
+
+var mysql = require('mysql');
+
+var mysqlconn = mysql.createConnection(configdata.mysqlconnection);
+app.use(cookieParser());
+app.use(cors());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+
+//Get all users API
+app.get('/users', function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Credentials', 'false')
+    res.setHeader('Access-Control-Expose-Headers', 'Content-Length, X-JSON');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+
+    dbmodel.getAllUsers(req, mysqlconn, function (result) {
+        res.json(result);
+    });
+});
+
+//Get user by id API
+app.get('/users/:id', function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Credentials', 'false')
+    res.setHeader('Access-Control-Expose-Headers', 'Content-Length, X-JSON');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+
+    dbmodel.getUser(req, mysqlconn, function (result) {
+        res.json(result);
+    });
+});
+
+//Get passttype API
+app.get('/passtype', function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Credentials', 'false')
+    res.setHeader('Access-Control-Expose-Headers', 'Content-Length, X-JSON');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+
+    dbmodel.getPassType(req, mysqlconn, function (result) {
+        res.json(result);
+    });
+});
+
+app.get('/getpasstype', function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Credentials', 'false')
+    res.setHeader('Access-Control-Expose-Headers', 'Content-Length, X-JSON');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    dbmodel.getPerticularPassType(req, mysqlconn, function (result) {
+        res.json(result);
+    });
+});
+
+//Login API
+app.post('/login', function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Credentials', 'false')
+    res.setHeader('Access-Control-Expose-Headers', 'Content-Length, X-JSON');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token');
+
+    /*var bdobj=JSON.parse(Object.keys(req.body)[0]);
+     console.log(bdobj['email']);
+     console.log(bdobj['password']);*/
+    //res.json();
+    dbmodel.doLogin(req, mysqlconn, function (result) {
+        res.json(result);
+    });
+});
+
+//BMTC id card creation API
+app.post('/createbmtcid', function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Credentials', 'false')
+    res.setHeader('Access-Control-Expose-Headers', 'Content-Length, X-JSON');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token');
+
+    dbmodel.createBMTCID(req, mysqlconn, function (result) {
+        res.json(result);
+    });
+});
+
+
+app.post('/register', function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Credentials', 'false')
+    res.setHeader('Access-Control-Expose-Headers', 'Content-Length, X-JSON');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token');
+
+    dbmodel.doRegistration(req, mysqlconn, function (result) {
+        res.json(result);
+    });
+});
+
+http.createServer(app).listen(2000, function (err) {
+    console.log('running server on port 2000');
+});
